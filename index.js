ebff16ef6a9e7861875cfd1b3c55c1cc693e61ad
KATORE DATTATRAY SAMPAT <dattakatore2021@gmail.com>
2024-10-22 14:41:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let age =25
+function displayAge(){
+    console.log("current age is" ,age)
+}
+function changeAge(newAge){
+    age=newAge
+    console.log("age has a updated" ,age)
+}displayAge()
+changeAge(30)
+displayAge()
